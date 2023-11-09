b6818eec0c99ff4b53b6a2ef8cdac0a5c9b92099
chirag kanani <107808217+kananichirag@users.noreply.github.com>
2023-11-09 14:01:25 +0530

    final

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use('/api/listing',ListingRoutes);
 app.use(express.static(path.join(__dirname,'/frontend/dist')))
 
 app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+    res.sendFile(path.join(__dirname,'frontend','index.html'))
 })
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
